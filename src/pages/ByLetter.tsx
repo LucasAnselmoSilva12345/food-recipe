9b33bbbb37cab331a8139a9534ef2e0ec7fd5a53
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-10-04 15:19:09 -0300

    Feat: Search Food by alphabet letter
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/ByLetter.tsx
+++ src/pages/ByLetter.tsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from 'react';
+import { FoodRandomProps } from '../types/FoodRandomProps';
+import { api } from '../api/meal-db-api';
+import { FoodCard } from '../components/FoodCard';
+
 export function ByLetter() {
+  const [selectedLetter, setSelectedLetter] = useState<string | null>(null);
+  const [foodResults, setFoodResults] = useState<FoodRandomProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchFoodByLetter = async () => {
+      if (selectedLetter) {
+        setLoading(true);
+        try {
+          const response = await api.get(`/search.php?f=${selectedLetter}`);
+          setFoodResults(response.data.meals || []);
+        } catch (error) {
+          console.error('Erro ao buscar dados da API:', error);
+        }
+        setLoading(false);
+      }
+    };
+    fetchFoodByLetter();
+  }, [selectedLetter]);
+
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  const handleLetterClick = (letter: string) => {
+    setSelectedLetter(letter);
+  };
+
   return (
-    <div>
-      <h1>By Letter</h1>
-    </div>
+    <section>
+      <h1>Pesquise por letra</h1>
+
+      <div className="space-x-2">
+        {alphabet.map((letter) => (
+          <button key={letter} onClick={() => handleLetterClick(letter)}>
+            {letter}
+          </button>
+        ))}
+      </div>
+
+      {loading && <p>Carregando</p>}
+      {!loading && foodResults.length === 0 && (
+        <p>Nenhum resultado encontrado ao pesquisar por esse letra</p>
+      )}
+      {!loading && foodResults.length > 0 && (
+        <div>
+          {foodResults.map((result) => (
+            <FoodCard
+              key={result.idMeal}
+              idMeal={result.idMeal}
+              strMealThumb={result.strMealThumb}
+              strMeal={result.strMeal}
+              strInstructions={result.strInstructions}
+              strYoutube={result.strYoutube}
+            />
+          ))}
+        </div>
+      )}
+      {foodResults.map((result) => (
+        <FoodCard
+          key={result.idMeal}
+          idMeal={result.idMeal}
+          strMealThumb={result.strMealThumb}
+          strMeal={result.strMeal}
+          strInstructions={result.strInstructions}
+          strYoutube={result.strYoutube}
+        />
+      ))}
+    </section>
   );
 }
