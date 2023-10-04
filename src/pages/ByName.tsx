cb2245653c65345967aac75dd4b9f78566a4a00a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-10-04 16:23:21 -0300

    Feat: Search by name style page
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/ByName.tsx
+++ src/pages/ByName.tsx
@@ -3,6 +3,7 @@ import { FoodRandomProps } from '../types/FoodRandomProps';
 import { api } from '../api/meal-db-api';
 import { FoodCard } from '../components/FoodCard';
 import { Title } from '../components/Title';
+import { MagnifyingGlass } from '@phosphor-icons/react';
 
 export function ByName() {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -18,17 +19,31 @@ export function ByName() {
   };
 
   return (
-    <section>
+    <section className="w-full md:w-4/5 lg:w-1/2 px-6 py-4">
       <Title title="Pesquise a receita pelo nome" />
-      <h1>Pesquise sua comida pelo nome</h1>
-      <div>
+      <div className="w-full flex flex-col mt-4 space-y-2">
+        <label
+          htmlFor="searchMeal"
+          className="text-neutral-800 font-inter font-medium"
+        >
+          Digite abaixo o nome do prato
+        </label>
         <input
           type="text"
+          name="searchMeal"
+          className="py-4 text-sm text-neutral-800 border-none rounded focus:outline focus:outline-2 focus:outline-orange-400"
+          placeholder="e.g. sushi"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
 
-        <button onClick={handleSearch}>pesquisar</button>
+        <button
+          className="flex items-center justify-center gap-1 py-3 text-base border-none bg-orange-500 text-orange-50 font-semibold"
+          onClick={handleSearch}
+        >
+          Pesquisar
+          <MagnifyingGlass size={20} className="text-orange-50" weight="bold" />
+        </button>
       </div>
 
       {foodData ? (
