bb05a3d22345250231b2fd7bc5f8f1b9fe3db6dc
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-10-04 14:54:04 -0300

    Feat: Search Food by name
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/ByName.tsx
+++ src/pages/ByName.tsx
@@ -1,7 +1,45 @@
+import { useState } from 'react';
+import { FoodRandomProps } from '../types/FoodRandomProps';
+import { api } from '../api/meal-db-api';
+import { FoodCard } from '../components/FoodCard';
+
 export function ByName() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [foodData, setFoodData] = useState<FoodRandomProps | null>(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await api.get(`/search.php?s=${searchTerm}`);
+      setFoodData(response.data.meals[0]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div>
-      <h1>By Name</h1>
-    </div>
+    <section>
+      <h1>Pesquise sua comida pelo nome</h1>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <button onClick={handleSearch}>pesquisar</button>
+      </div>
+
+      {foodData ? (
+        <FoodCard
+          idMeal={foodData.idMeal}
+          strMealThumb={foodData.strMealThumb}
+          strMeal={foodData.strMeal}
+          strInstructions={foodData.strInstructions}
+          strYoutube={foodData.strYoutube}
+        />
+      ) : (
+        <p>Não foi possivel encontrar a receita</p>
+      )}
+    </section>
   );
 }
